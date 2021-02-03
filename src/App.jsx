7004cf3ce213a6c1a766a23d47d4cf8984c6e2ad
RubenMontes <rubenmontes330@gmail.com>
2021-02-03 19:14:32 +0000

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './App.css'
+const App = () => {
+    return (
+        <div className='react-calculator'>
+            Calc App
+
+        </div>)
+}
+export default App
